366d4fde13443f3c0090a06e959b52d723f68c39
Larysa Koziarivska <ryssia@ukr.net>
2018-11-16 09:49:21 +0200

    Задача №2 готова

--- hw2/index.php
+++ hw2/index.php
@@ -1,3 +1,5 @@
 <?php
 
-require(‘src/functions.php’);
+require('src/functions.php');
+
+task2('+', 3, 5, 10);
--- hw2/src/functions.php
+++ hw2/src/functions.php
@@ -1,4 +1,5 @@
 <?php
+
 // #1
 
 function task1($string)
@@ -11,13 +12,52 @@ function task1($string)
         }
     } else {
         if ($arg_array[1] === true) {
+            $result = '';
             for ($i=0; $i<=sizeof($string); ++$i) {
-                $string[$i] .= $string[$i];
+                $result .= $string[$i];
             }
-            return $string;
+            echo $result;
         }
     }
 }
 
 // #2
 
+function task2()
+{
+    $arg_array = func_get_args();
+    $result = $arg_array[1];
+    $expres = $arg_array[1];
+    switch ($arg_array[0]) {
+        case '+':
+            for ($i=2; $i<sizeof($arg_array); ++$i) {
+                $result += $arg_array[$i];
+                $expres = $expres . ' + ' .$arg_array[$i];
+            };
+            echo $expres . ' = ' .$result;
+            break;
+        case '-':
+            for ($i=2; $i<sizeof($arg_array); ++$i) {
+                $result -= $arg_array[$i];
+                $expres .= ' - ' .$arg_array[$i];
+            };
+            echo $expres . ' = ' .$result;
+            break;
+        case '*':
+            for ($i=2; $i<sizeof($arg_array); ++$i) {
+                $result *= $arg_array[$i];
+                $expres .= ' * ' .$arg_array[$i];
+            };
+            echo $expres . ' = ' .$result;
+            break;
+        case '/':
+            for ($i=2; $i<sizeof($arg_array); ++$i) {
+                $result /= $arg_array[$i];
+                $expres .= ' / ' .$arg_array[$i];
+            };
+            echo $expres . ' = ' .$result;
+            break;
+        default:
+            echo "Первый аргумент не обозначает арифметическое действие";
+    }
+}
